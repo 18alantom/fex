48277eff89fce6d407c3f4e3f1cfad1d4d9df346
18alantom <2.alan.tom@gmail.com>
2024-05-01 23:05:52 +0530

    add simple release to build for linux

--- build.zig
+++ build.zig
@@ -1,6 +1,20 @@
 const std = @import("std");
 
-pub fn build(b: *std.Build) void {
+const release_queries: []const std.Target.Query = &.{
+    .{
+        .cpu_arch = .x86_64,
+        .os_tag = .linux,
+        .abi = .gnu,
+    },
+    .{
+        // FIXME: macos doesn't compile with these
+        // .cpu_arch = .aarch64,
+        // .os_tag = .macos,
+        // .abi = .none,
+    },
+};
+
+pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
@@ -32,4 +46,26 @@ pub fn build(b: *std.Build) void {
     // const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
     // const test_step = b.step("test", "Run unit tests");
     // test_step.dependOn(&run_exe_unit_tests.step);
+
+    // zig build release
+    const release_step = b.step("release", "Build release binaries");
+    for (release_queries) |query| {
+        const release_exe = b.addExecutable(.{
+            .name = "fex",
+            .root_source_file = b.path("main.zig"),
+            .target = b.resolveTargetQuery(query),
+            .optimize = .ReleaseSafe,
+            .single_threaded = true,
+        });
+
+        release_exe.linkLibC();
+
+        const custom_dest_dir = try query.zigTriple(b.allocator);
+        const install_step = b.addInstallArtifact(release_exe, .{
+            .dest_dir = .{
+                .override = .{ .custom = custom_dest_dir },
+            },
+        });
+        release_step.dependOn(&install_step.step);
+    }
 }
