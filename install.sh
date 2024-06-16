e658147557f84d68845c2b0cfed75fc44608d837
18alantom <2.alan.tom@gmail.com>
2024-06-16 14:03:09 +0530

    add install.sh (w.i.p)

--- install.sh
+++ install.sh
@@ -3,20 +3,38 @@
 set -euo pipefail
 
 version=0.0.1
+
+# Styles
+red="\x1b[31m"
+yellow="\x1b[33m"
+faint="\x1b[2m"
+reset="\x1b[m"
+
 # TODO:
-# - Check arch, os
-# - Download correct release depending on os, arch
-# -
-#
+# - Cleanup on error
+# - Style sucess messsages
+# - Add setup complete
+# - Test script
 
 function install() {
-  tar_name=$(get_tar_name)
-  echo "File name will be $tar_name"
-}
+  if [[ $(can_install) == "n" ]]; then
+    error "Installation cancelled"
+  fi
 
-function error() {
-  echo $1
-  exit 1
+  local tar_name=$(get_tar_name)
+
+  echo "Downloading $tar_name from GitHub"
+  local folder_name=$(download $tar_name)
+  cd $folder_name
+  place_binary
+
+  if [[ $(ask "Setup fex default command?") == "y" ]]; then
+    setup_defaults
+  fi
+  
+  if [[ $(which_shell) == "zsh" && $(ask "Setup zsh configuration?") == "y" ]]; then
+    setup_zsh
+  fi
 }
 
 function get_tar_name() {
@@ -31,7 +49,166 @@ function get_tar_name() {
   echo "fex-$version-$target_stub.tar.gz"
 }
 
-# function can_install() {
+function can_install() {
+  if ! command -v "fex" &> /dev/null; then
+    echo "y"
+    return
+  fi
 
-# }
+  local bin_path=$(command -v "fex")
+  local installed_version=$($bin_path --version)
+
+  ask "Found installed $installed_version, overwrite with $version?"
+}
+
+function download() {
+  local tar_name=$1
+
+  if ! command -v "curl" &> /dev/null; then
+    error "File cannot be downloaded, curl not found"
+  fi
+  
+  local file_name="fex.tar.gz"
+  if [[ -f $file_name ]]; then
+    rm $file_name
+  fi
+
+  local url="https://github.com/18alantom/fex/releases/download/v$version/$tar_name"
+  curl -fsSL $url --output $file_name
+  tar --no-same-owner -xzf $file_name
+  rm $file_name
+  
+  local folder_name=${tar_name%.tar.gz}
+  if [[ ! -d $folder_name ]]; then
+    error "$folder_name not found after downloading"
+  fi
+
+  echo $folder_name
+}
+
+function place_binary() {
+  if [[ ! -f "fex" ]]; then
+    error "Binary (fex) not found at $(pwd)"
+  fi
+
+  chmod +x fex
+  local output=$(./fex --version 2>&1)
+
+  if [[ $? -ne 0 ]]; then
+    error "Invalid binary, error: $output"
+  fi
+  
+  if [[ "$output" != "$version" ]]; then
+    error "Invalid version: $output"
+  fi
+  
+  mv ./fex /usr/local/bin/fex
+}
+
+
+function setup_defaults() {
+  local default_command="export FEX_DEFAULT_COMMAND=\"fex"
+
+  # Show icons
+  if [[ $(ask "Display icons?") == "n" ]]; then
+    default_command+=" --no-icons"
+  fi
+  
+  # Show item size
+  if [[ $(ask "Display size?") == "n" ]]; then
+    default_command+=" --no-size"
+  fi
+
+  # Show perm info
+  if [[ $(ask "Display permissions?") == "n" ]]; then
+    default_command+=" --no-perm"
+  fi
+
+  # Show time
+  local display_time=$(ask "Display time?")
+  if [[ $display_time == "n" ]]; then
+    default_command+=" --no-time"
+  fi
+  
+  # Which time to set, defaults to changed.
+  if [[ $display_time == "y" && $(ask "Show modified time?") == "y" ]]; then
+    default_command+=" --time modified"
+  elif [[ $display_time == "y" && $(ask "Show accessed time?") == "y" ]]; then
+    default_command+=" --time accessed"
+  elif [[ $display_time == "y" ]]; then
+   default_command+=" --time changed"
+  fi
+
+  default_command+="\""
+
+  local rc_path=$(which_rc)
+  if old_command=$(grep "export FEX_DEFAULT_COMMAND=" $rc_path); then
+    sed -i="" "s/$old_command/$default_command/" $rc_path
+  else
+    echo $default_command >> $rc_path
+  fi
+}
+
+
+function setup_zsh() {
+  if [[ ! -f ".fex.zsh" ]]; then
+    error "Zsh file (.fex.zsh) not found at $(pwd)"
+  fi
+  
+  cp ./.fex.zsh ~/.fex.zsh
+  
+  local rc_path=$(which_rc)
+  local load_widget="[ -f ~/.fex.zsh ] && source ~/.fex.zsh"
+  
+  if ! grep -q "$load_widget" $rc_path; then
+    echo $load_widget >> $rc_path
+  fi
+  
+  # Key binding already exists
+  if grep -q "^bindkey.*fex-widget" $rc_path; then
+    return
+  fi
+
+  local bind_ctrlf="bindkey '^f' fex-widget"
+  if $(ask "Bind CTRL-F to invoke fex?") == "y"; then
+    echo $bind_ctrlf >> $rc_path
+  fi
+}
+
+function which_rc() {
+  case $(which_shell) in
+    "zsh")  echo -e "~/.zshrc"  ;;
+    "bash") echo -e "~/.bashrc" ;;
+  esac
+}
+
+function which_shell() {
+  local shells="zsh bash"
+  for s in $shells; do
+    if echo "$SHELL" | grep -q "$s"; then
+      echo "zsh"
+      break
+    fi
+  done
+}
+
+function error() {
+  echo -e "\x1b[31m$1\x1b[m"
+  exit 1
+}
+
+function ask() {
+  read -p "$(echo -e "$yellow>$reset $1 $faint([n]/y)$reset: ")" -r
+  if [[ $REPLY =~ ^[Yy]$ ]]; then
+    echo "y"
+    return
+  fi
+  
+  echo "n"
+}
+
+function cleanup() {
+ #TODO: complete this
+}
+
 install
